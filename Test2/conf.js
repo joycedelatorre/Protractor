17dc56efe01bb142ece7ec3f1fd0b5421ec65e1f
joyce <joycedelatorre87@gmail.com>
2018-06-07 15:11:45 -0500

    changing the configuration to multiple browsers

--- Test2/conf.js
+++ Test2/conf.js
@@ -1,6 +1,28 @@
+// // conf.js
+// exports.config = {
+//     framework: 'jasmine',
+//     seleniumAddress: 'http://localhost:4444/wd/hub',
+//     specs: ['spec.js']
+//   }
+
+// conf.js STEP 3 CHANGING THE CONFIGURATION: The configuration file lets you change things like which browsers are used and how to connect to the Selenium Server.
+// exports.config = {
+//   framework: 'jasmine',
+//   seleniumAddress: 'http://localhost:4444/wd/hub',
+//   specs: ['spec.js'],
+//   capabilities: {
+//     browserName: 'firefox'
+//   }
+// }
+
 // conf.js
 exports.config = {
-    framework: 'jasmine',
-    seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['spec.js']
-  }
+  framework: 'jasmine',
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  specs: ['spec.js'],
+  multiCapabilities: [{
+    browserName: 'firefox'
+  }, {
+    browserName: 'chrome'
+  }]
+}
